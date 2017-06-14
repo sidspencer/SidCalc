17c3d9cd4f3edee8f2d1e1b0d6d1059c4057750f
Sid Spencer <dis@bub.local>
2017-06-13 19:51:36 -0500

    Add comments, update name of templateUrl file.

--- components/CalcButton.js
+++ components/CalcButton.js
@@ -1,13 +1,17 @@
+/**
+ * Directive for creating calculator buttons for SidCalc
+ */
 angular.module('SidCalcApp').directive('calcButton', function CalcButtonDirective() {
       return {
         restrict: 'E',
-        templateUrl: 'components/calc_button.html',
+        templateUrl: 'components/CalcButton.html',
         scope: {
           type: '@',
           text: '@',
           click: '&'
         },
         link: function CalcButtonController($scope) {
+            // Wrap the click function.
             $scope.doClick = function doClick(item) {
               $scope.click()($scope.text);
             }

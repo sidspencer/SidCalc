d0715a218217223f799db29f52ad3a83765c9159
Sid Spencer <dis@bub.local>
2017-06-13 19:53:08 -0500

    Add comments.

--- components/CalcScreen.js
+++ components/CalcScreen.js
@@ -1,3 +1,6 @@
+/**
+ * Directive for the screen of the SidCalc calculator.
+ */
 angular.module('SidCalcApp').directive('calcScreen', function CalcScreenDirective() {
       return {
         restrict: 'E',
